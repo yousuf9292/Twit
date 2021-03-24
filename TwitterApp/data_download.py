15aa2e52559be276f73e07aaeec5736197e0aba7
Wes House <wesevan7@gmail.com>
2021-03-24 21:28:30 +0100

    Added explanatory sections in streamlit as well as user test for sentiment analysis

--- TwitterApp/data_download.py
+++ TwitterApp/data_download.py
@@ -9,6 +9,7 @@ import streamlit as st
 # Twiterr handles of selected left-leaning US/UK publishing houses
 
 def csv_downloader(data, new_filename):
+    # Generates and names a link to download the given object_to_download
     csvfile = data.to_csv()
     b64 = base64.b64encode(csvfile.encode()).decode()
     csv_filename = f"{new_filename}_tweets.csv"
@@ -16,24 +17,4 @@ def csv_downloader(data, new_filename):
     href = f'<a href="data:file/csv;base64,{b64}" download="{csv_filename}">Click Here!!</a>'
     st.markdown(href,unsafe_allow_html=True)
 
-# def download_link(object_to_download, download_filename, download_link_text):
-#     """
-#     Generates a link to download the given object_to_download.
-
-#     object_to_download (str, pd.DataFrame):  The object to be downloaded.
-#     download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
-#     download_link_text (str): Text to display for download link.
-
-#     Examples:
-#     download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
-#     download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
-
-#     """
-#     if isinstance(object_to_download, pd.DataFrame):
-#         object_to_download = object_to_download.to_csv(index=False)
-
-#     # some strings <-> bytes conversions necessary here
-#     b64 = base64.b64encode(object_to_download.encode()).decode()
-
-#     return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
--- TwitterApp/interface.py
+++ TwitterApp/interface.py
@@ -5,6 +5,7 @@ from data_download import csv_downloader
 import numpy as np
 import pandas as pd
 import nltk
+import time
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 nltk.download('vader_lexicon')
@@ -13,16 +14,16 @@ sid = SentimentIntensityAnalyzer()
 header = st.beta_container()
 dataset = st.beta_container()
 features = st.beta_container()
-model = st.beta_container()
+
+progress = st.progress(0)
 
 
 with header:
-    st.title("Twitter Sentiment Retrieval")
+    st.title("🔵 Twitter Sentiment Retrieval 📲")
     user_input = st.text_input("Enter a Twitter Handle (Do not include @)")
     if user_input:
         try:
             handle = user_input
-
             get_all_tweets(handle)
             handle_tweets_df = pd.read_csv(f"new_{handle}_tweets.csv")
             handle_tweets_df["text"] = handle_tweets_df["text"].map(remove_rt)
@@ -35,19 +36,52 @@ with header:
             st.write(handle_tweets_df)
             csv_downloader(handle_tweets_df, handle)
         except:
-            st.error("Sorry")
+            st.error("Invalid handle or tweets unavailable. Please try another handle.")
 
 
 with dataset:
-    pass
+    st.subheader("About this application")
+    st.markdown("This is a simple web application that grabs approximately 3,000\
+     recent tweets of any Twitter user and runs them through a sentiment intensity analyser.")
 
+    st.subheader("How it's done")
+    st.markdown("Using the Twitter API, the application is able to retrieve any\
+      publicly available twitter account and it's respective tweets. Once the\
+      tweets are loaded into a dataframe they are passed through a sublibary\
+       within Natural Language Toolkit (NLTK), a suite of libraries within \
+       natural language processing. The current model only works for Tweets \
+       in English and does not process attachements to tweets such as\
+       images or hyperlinks. More information about the Twitter API \
+       can be found here: https://developer.twitter.com/en/docs. For further\
+       reading about NLTK's sentiment intensity analyzer library, read here:\
+       https://www.nltk.org/howto/sentiment.html.")
+
+    st.subheader("Who is this for?📈 📉 ")
+    st.markdown("Anyone! But those who work with or are interested in data analysis \
+      may find this most useful, as well as companies, influencers, or any user \
+      curious to see their sentiment score. Is there a correlation \
+       between engagement and positivity? Negativity? Maybe time of day plays a \
+       role in how a negative tweet is received by followers? There are many \
+       questions one could seek to answer with this data.")
+
+    st.subheader("What is sentiment analysis?😡😐😁")
+    st.markdown("Put simply, sentiment analysis is the process of a computer \
+      identifying or categorizing opinions from a piece of text and \
+      determining whether the writer's attitude towards a particular \
+      subject is positive, negative or neutral. Here's a definitive guide\
+      on the subject: https://monkeylearn.com/sentiment-analysis/")
 
 with features:
-    pass
-
-with model:
-    pass
-
+    st.subheader("Try it out")
+    user_input = st.text_input("Write a sentence and I'll try to guess how you \
+      feel (note: computer's are not so good with sarcasm 🙃)")
+    if user_input:
+        polarity = sid.polarity_scores(user_input)
+        if polarity["compound"] >= 0:
+            st.balloons()
+            st.success("🤖: I'm detecting positivity!!!")
+        else:
+            st.error("🤖: I'm detecting negativity")
 
 # st.markdown("""# This is a header
 # ## This is a sub header
